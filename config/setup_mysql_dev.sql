95d3eb6681724b59692490ddf545f5df2c99bc49
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-17 16:57:46 +0100

    updated gitignore file

--- config/setup_mysql_dev.sql
+++
@@ -1,7 +0,0 @@
--- Prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS quizzer_dev_db;
-CREATE USER IF NOT EXISTS 'quizzer_dev'@'localhost' IDENTIFIED BY 'quizzer_dev_pwd';
-GRANT ALL PRIVILEGES ON `quizzer_dev_db`.* TO 'quizzer_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'quizzer_dev'@'localhost';
-FLUSH PRIVILEGES;
