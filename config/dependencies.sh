71875a8b1bd1975b3e84c83b6b395567f9910ab8
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-17 21:41:36 +0100

    created script for dependencies

---
+++ config/dependencies.sh
@@ -0,0 +1,20 @@
+# Install Gunicorn
+pip install gunicorn
+
+# Install Flask
+pip install flask
+
+# Install Flask-Cors
+pip install flask-cors
+
+# Install SQLAlchemy
+pip install sqlalchemy
+
+# Install SQLClient
+sudo apt install -y python3-dev default-libmysqlclient-dev build-essential
+
+# Install MySQL
+sudo apt install -y mysql-server
+
+# Install Nginx
+sudo apt install -y nginx
